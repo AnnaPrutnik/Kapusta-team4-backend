fb77b01a8d3ab1e16e06f8cec84cbe05c58fc873
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-02-05 18:20:07 +0200

    feat: add user & abstract repositories

---
+++ repositories/index.js
@@ -0,0 +1,2 @@
+export { UserRepository } from './user-repository/user-repository';
+export { AbstractRepository } from './abstract-repository/abstract-repository';
---
+++ repositories/user-repository/user-repository.js
@@ -0,0 +1,7 @@
+import { AbstractRepository } from '../';
+
+export class UserRepository extends AbstractRepository {
+  constructor(model) {
+    super(model);
+  }
+}
