f99068573cbedb7c56a50a3f64e59470e92898ca
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-02-05 15:51:09 +0200

    build: change cjs modules to es6 modules

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
 
 const app = express();
 
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
 app.use((req, res) => {
-  res.status(404).json({message: 'Not found'});
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
   const params = req.params;
-  res.status(500).json({params, message: err.message});
+  res.status(500).json({ params, message: err.message });
 });
 
-module.exports = app;
+export default app;
