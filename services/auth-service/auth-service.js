d4ab3b58856ddd212497c0504f78a4006453127c
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-02-08 22:24:58 +0200

    feat: add setting token for user and check token validity

--- services/auth-service/auth-service.js
+++ services/auth-service/auth-service.js
@@ -15,15 +15,34 @@ export class AuthService {
       return { firstName, lastName, email, verifyToken };
     } catch (error) {
       console.error(`Error on creating new User: ${error.message}`);
+      return null;
     }
   }
 
-  async getToken(user) {
+  getToken(user) {
     const { id, email } = user;
     const token = jwt.sign({ id, email }, JWT_KEY, { expiresIn: '8h' });
-    console.log(token);
     return token;
   }
 
-  async setToken() {}
+  async setToken(id, token) {
+    try {
+      await this.repository.updateToken(id, token);
+    } catch (error) {
+      console.error(`Error on updating token: ${error.message}`);
+      return null;
+    }
+  }
+
+  async isTokenValid(token) {
+    try {
+      const decodedToken = jwt.verify(token, JWT_KEY);
+      if (decodedToken) {
+        return true;
+      }
+    } catch (error) {
+      console.error(`Error on checking token: ${error.message}`);
+      return null;
+    }
+  }
 }
