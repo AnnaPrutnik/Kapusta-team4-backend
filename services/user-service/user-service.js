13089ad3ec24f6a9589f922b1af1c2864ba3a6bc
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-02-08 16:43:09 +0200

    feat: add method to search user in user service

--- services/user-service/user-service.js
+++ services/user-service/user-service.js
@@ -4,4 +4,13 @@ export class UserService {
   constructor(repository) {
     this.repository = repository;
   }
+
+  async findUserByEmail(email) {
+    try {
+      return await this.repository.findUserByEmail(email);
+    } catch (error) {
+      console.error(`Error on searching user: ${error.message}`);
+      return null;
+    }
+  }
 }
