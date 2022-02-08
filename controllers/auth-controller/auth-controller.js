11f29035957c54c087e7297f9d5dd1604c959eb1
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-02-08 18:19:09 +0200

    feat: add user signup controller

--- controllers/auth-controller/auth-controller.js
+++ controllers/auth-controller/auth-controller.js
@@ -2,8 +2,24 @@ import { userService, authService } from '../../services';
 import createError from 'http-errors';
 class AuthController {
   async signUpUser(req, res, next) {
-    // example error handling
-    return next(createError(401, 'Custom Error in Controller'));
+    const { email } = req.body;
+    const user = await userService.findUserByEmail(email);
+    if (user) {
+      return next(createError(409, 'Email in Use'));
+    }
+
+    const userData = await authService.signUpUser(req.body);
+
+    if (!userData) {
+      return next(createError());
+    }
+
+    // TODO add mailer service
+    return res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: userData,
+    });
   }
 
   async logInUser(req, res, next) {}
