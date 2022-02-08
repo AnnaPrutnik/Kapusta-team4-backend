187fa08bcde1b30a65728d487fbb07c23e9f6540
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-02-08 16:41:52 +0200

    feat: add repository methods to create new user and to find existing user by email

--- repositories/user-repository/user-repository.js
+++ repositories/user-repository/user-repository.js
@@ -4,4 +4,12 @@ export class UserRepository extends AbstractRepository {
   constructor(model) {
     super(model);
   }
+
+  async findUserByEmail(email) {
+    return await this.model.findOne({ email });
+  }
+
+  async createNewUser(data) {
+    return await this.model(data).save();
+  }
 }
