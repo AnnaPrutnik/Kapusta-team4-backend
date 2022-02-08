3376a962f24306483c8c6c7f3e174797e12a23a3
DmitryDr-dev <dmitrydr.dev@gmail.com>
2022-02-08 16:57:01 +0200

    feat: add signUpUser method in auth service to sign up new users

--- services/auth-service/auth-service.js
+++ services/auth-service/auth-service.js
@@ -4,4 +4,14 @@ export class AuthService {
   constructor(repository) {
     this.repository = repository;
   }
+
+  async signUpUser(credentials) {
+    try {
+      const { firstName, lastName, email, verifyToken } =
+        await this.repository.createNewUser(credentials);
+      return { firstName, lastName, email, verifyToken };
+    } catch (error) {
+      console.error(`Error on creating new User: ${error.message}`);
+    }
+  }
 }
