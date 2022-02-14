f0b4289a7075eb55dc47d588e0947addecff8b11
DMITRIJ SHEVCHUK <79271465+AmbassadorOfHappiness@users.noreply.github.com>
2022-02-14 19:38:57 +0200

    photo and name change

--- controllers/user-controller/user-controller.js
+++ controllers/user-controller/user-controller.js
@@ -1,5 +1,9 @@
 import createError from 'http-errors';
 import { userService } from '../../services';
+import {  
+  UploadFileService,
+  CloudFileStorage
+} from '../../services/file-storage';
 
 class UserController {
   async getBalance(req, res, next) {
@@ -24,6 +28,32 @@ class UserController {
       code: 200,
       data: balance,
     });
+  }
+
+  async uploadAvatar(req, res, next) {
+    const user = res.locals.user
+    const uploadService = new UploadFileService(
+      CloudFileStorage,
+      req.file,
+      user
+    )
+    const avatarUrl = await uploadService.updateAvatar();
+  
+    res
+      .status(200)
+      .json({ status: 'success', code: 200, data: {avatarUrl} });
+  }
+
+  async updateUser(req, res, next) {
+    const { id } = res.locals.user;
+    // const { id: userId } = req.user;
+    const user = await userService.updateUser(id, req.body);
+    if (!user) {
+      return next(createError(404, 'Not found'));
+    }
+    res
+      .status(200)
+      .json({ status: 'success', code: 200, data: { user } })
   }
 }
 
